ba95b275c554ecc967a254f518881372102522e8
Tim Riemer <Tim.Riemer@fntsoftware.com>
2018-07-02 20:04:16 +0200

    add PostgreSQLContainerTest including Gradle dependencies

---
+++ src/test/java/mobi/riemer/PostgreSQLContainerTest.java
@@ -0,0 +1,47 @@
+package mobi.riemer;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class PostgreSQLContainerTest {
+
+    private static PostgreSQLContainer postgres = new PostgreSQLContainer();
+
+    @BeforeAll
+    static void startUp() {
+        postgres.start();
+    }
+
+    @AfterAll
+    static void tearDown() {
+        postgres.stop();
+    }
+
+    @Test
+    void testSimpleSQL() throws SQLException {
+        // HikariConfig
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(postgres.getJdbcUrl());
+        hikariConfig.setUsername(postgres.getUsername());
+        hikariConfig.setPassword(postgres.getPassword());
+
+        HikariDataSource ds = new HikariDataSource(hikariConfig);
+        Statement statement = ds.getConnection().createStatement();
+        statement.execute("SELECT 1");
+        ResultSet resultSet = statement.getResultSet();
+
+        resultSet.next();
+        assertEquals(1, resultSet.getInt(1));
+    }
+}
