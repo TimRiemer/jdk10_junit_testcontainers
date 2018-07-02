9c35ad1703acf35f1e115fd043b42ca073d8a893
Tim Riemer <Tim.Riemer@fntsoftware.com>
2018-07-02 17:10:46 +0200

    change WebdriverContainerTest to JUnit 4 Rule

--- src/test/java/mobi/riemer/WebdriverContainerTest.java
+++ src/test/java/mobi/riemer/WebdriverContainerTest.java
@@ -1,13 +1,13 @@
 package mobi.riemer;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.Rule;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testcontainers.containers.BrowserWebDriverContainer;
 import org.testcontainers.containers.DefaultRecordingFileFactory;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.VncRecordingContainer;
 
 import java.io.File;
 import java.util.concurrent.TimeUnit;
@@ -15,27 +15,16 @@ import java.util.concurrent.TimeUnit;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.testcontainers.containers.BrowserWebDriverContainer.VncRecordingMode.RECORD_ALL;
 
-class WebdriverContainerTest {
+public class WebdriverContainerTest {
 
-    private static BrowserWebDriverContainer chrome = new BrowserWebDriverContainer()
+    @Rule
+    public BrowserWebDriverContainer chrome = new BrowserWebDriverContainer()
             .withDesiredCapabilities(DesiredCapabilities.chrome())
-            .withRecordingMode(RECORD_ALL, new File("./build/"))
-            .withRecordingFileFactory(new DefaultRecordingFileFactory());
-
-    @BeforeAll
-    static void startUp() {
-        chrome.start();
-    }
-
-    @AfterAll
-    static void tearDown() {
-        chrome.stop();
-    }
+            .withRecordingMode(RECORD_ALL, new File("./build/"));
 
     @Test
-    void searchForTestcontainersOnGoogle() {
+    public void searchForTestcontainersOnGoogle() {
         var driver = chrome.getWebDriver();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
         driver.get("http://www.google.com");
         driver.findElement(By.name("q")).sendKeys("testcontainers");
